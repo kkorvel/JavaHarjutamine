ebb86c6bc319cf840b31e81e389de0c14ff640b6
kkorvel <kkorvel@itcollege.ee>
2017-11-27 20:02:59 +0200

    edasi!

--- HarjutamineJava/src/Array.java
+++ HarjutamineJava/src/Array.java
@@ -13,10 +13,28 @@ public class Array {
         Scanner klaviatuur = new Scanner(System.in);
         System.out.println("Mida sa soovid osta! Kas Apelsini, Kohvi, Mett, Hapukoort?");
         String vastus = klaviatuur.next();
-        System.out.println("Tere! Sa ostid just " + vastus);
+        System.out.println(vastus);
+        //Stringide compare on equals abil
+        //https://stackoverflow.com/questions/10924561/java-scanner-string-input-if-statement-not-working
+        //Why, you ask?
+        //== checks to see if the actual object references are the same.
+        //equals(...) checks if the two Strings hold the same string (ie the same characters in the same order)
+        //debug näitab kenasti
+
+        if (vastus.equals("Mett")){
+            System.out.println("Mesi on hea!(equals) ");
         }
-
-
+        else {
+            System.out.println("ei ole hea");
+        }
+        if (vastus == "Mett"){
+            System.out.println("Mesi on hea!( == )");
+        }
+        System.out.println("Tere! Sa ostid just " + vastus);
     }
+
+
+
+}
 
 
