f0423afce9f0ab7b6847e7a1c412eaec019306c7
kkorvel <koit.korvel@gmail.com>
2018-06-21 15:49:46 +0300

    Update Person.java

--- HarjutamineJava/src/Person.java
+++ HarjutamineJava/src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //klass
 public class Person {
     public String getName() {
@@ -31,6 +33,8 @@ public class Person {
         Tervitus("Kotu");
         Returnija(11);
         Testing(0);
+        Arvuti();
+        Arvutajad(5,9);
     }
     public static void Tervitus(String name){
         System.out.println("Tere!" + name);
@@ -51,6 +55,23 @@ public class Person {
         }
 
         }
+    }
+    public static void Arvuti(){
+        int a;
+        int b;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Mis on a väärtus?");
+        a = sc.nextInt();
+        System.out.println("Mis on b väärtus?");
+        b = sc.nextInt();
+        int vastus = a + b;
+        System.out.println("Sinu vastuseks on: " + vastus);
+    }
+    public  static Integer  Arvutajad(int a, int b){
+        int vastus = a + b;
+        System.out.println("Vastuseks on: " + vastus);
+        return vastus;
     }
 
 }
